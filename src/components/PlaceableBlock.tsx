44eb60acf384e0b6113d456d317313d27494b131
daniel.corner <danielcorner7@gmail.com>
2021-11-23 01:04:26 -0500

    checkpoint

--- src/components/PlaceableBlock.tsx
+++ src/components/PlaceableBlock.tsx
@@ -1,37 +1,133 @@
-import { Interactive, useHitTest, useXRFrame } from "@react-three/xr";
+import {
+  Interactive,
+  useHitTest,
+  useInteraction,
+  useXRFrame,
+} from "@react-three/xr";
 import { Box, Html } from "@react-three/drei";
 import { useRef, useState } from "react";
-import { useGeolocation } from "react-use";
+import { useGeolocation, useInterval } from "react-use";
 import { Icon } from "@mui/material";
 import { LocationOff } from "@mui/icons-material";
 import { useCubes } from "../store";
 import { BOX_WIDTH } from "../utils/constants";
+import { useThree } from "@react-three/fiber";
 
 export function PlaceableBlock() {
   const ref = useRef<any>(null);
-  useHitTest((hit) => {
+
+  // useHitTest((hit) => {
+  //   if (!ref.current) {
+  //     return;
+  //   }
+  //   /* const nextPosition =  */ hit.decompose(
+  //     ref.current.position,
+  //     ref.current.rotation,
+  //     ref.current.scale
+  //   );
+  //   // console.log("🌟🚨 ~ useHitTest ~ nextPosition", nextPosition);
+
+  //   // move to nearest grid position after hit test
+  //   // const newPosition = getNearestGridPosition(ref.current.position);
+  //   // console.log("🌟🚨 ~ useHitTest ~ newPosition", newPosition);
+  //   // ref.current.position = newPosition;
+  // });
+
+  // https://github.com/pmndrs/react-xr/issues/40#issuecomment-803566109
+  //   useHitTest hook will call the callback function once it detects a real surface. It will pass two arguments: a matrix and XRHitTestResult
+
+  // Let's look at the simplest example
+
+  // function HitTestExample() {
+  //   const ref = useResource()
+
+  //   useHitTest((hit) => {
+  //     hit.decompose(ref.current.position, ref.current.rotation, ref.current.scale)
+  //   })
+
+  //   return <Box ref={ref} args={[0.1, 0.1, 0.1]} />
+  // }
+  // in this example we want to place a box on a real surface every frame
+
+  // useHitTest((hit) => {
+  //   if (!ref.current) {
+  //     return;
+  //   }
+  //   // Here hit is a Matrix4 object which contains position and rotation of a position on a real surface.
+
+  //   // Next, we can copy position of the hit to the our Box mesh
+  //   hit.decompose(
+  //     ref.current.position,
+  //     ref.current.rotation,
+  //     ref.current.scale
+  //   );
+
+  //   // the effect of this is that as soon as there's a real plane detected, on every frame the Box will be placed on that plane
+
+  //   // Here is the working example on codesandbox: https://codesandbox.io/s/react-xr-usehittest-demo-5iff9?file=/src/App.tsx
+
+  //   // ...
+  //   // ...finally, we can find the nearest position in our grid
+  //   // const newPosition = getNearestGridPosition(ref.current.position);
+
+  //   // ref.current.position = newPosition;
+
+  //   console.log(
+  //     "🌟🚨 ~ useHitTest ~ ref.current.rotation",
+  //     ref.current.rotation
+  //   );
+  // });
+
+  const { camera } = useThree();
+  useXRFrame((time: number, xrFrame) => {
+    console.log("🌟🚨 ~ useXRFrame ~ xrFrame", xrFrame);
+    // const seconds = time / 1000;
+    // const go = () % 1 === 0;
+    console.log("🌟🚨 ~ useXRtime ~ time", time);
     if (!ref.current) {
       return;
     }
-    /* const nextPosition =  */ hit.decompose(
-      ref.current.position,
-      ref.current.rotation,
-      ref.current.scale
-    );
-    // console.log("🌟🚨 ~ useHitTest ~ nextPosition", nextPosition);
-
-    // move to nearest grid position after hit test
-    // const newPosition = getNearestGridPosition(ref.current.position);
-    // console.log("🌟🚨 ~ useHitTest ~ newPosition", newPosition);
-    // ref.current.position = newPosition;
+    const newPosition = [
+      camera.position.x,
+      camera.position.y,
+      camera.position.z - 2,
+    ];
+    console.log("🌟🚨 ~ useXRFrame ~ newPosition", newPosition);
+    ref.current.position.set(newPosition);
   });
 
+  // useInterval(() => {
+  //   if (!ref.current) {
+  //     return;
+  //   }
+
+  //   const newPosition = getNearestGridPosition([
+  //     camera.position.x - 0.1,
+  //     camera.position.y + 0.1,
+  //     camera.position.z + 1,
+  //   ]);
+  //   ref.current.position.set(newPosition);
+  //   console.log("🌟🚨 ~ useXRFrame ~ newPosition", newPosition);
+  // }, 1000);
+
   const [isHovered, setIsHovered] = useState(false);
-  const [, setCubes] = useCubes();
+  const [cubes, setCubes] = useCubes();
 
   const handleSelect = () => {
+    if (!ref.current) {
+      return;
+    }
     console.log("🚨 ~ HitTestExample ~ handleSelect");
-    const newCube = { position: ref.current.position };
+    const newPosition = getNearestGridPosition(ref.current.position);
+    console.log("🌟🚨 ~ handleSelect ~ newPosition", newPosition);
+    const newCube = { position: newPosition };
+    const alreadyExists =
+      Array.from(new Set([...cubes, newCube])).length === cubes.length;
+
+    if (alreadyExists) {
+      console.log("🌟🚨 ~ handleSelect ~ alreadyExists", alreadyExists);
+      return;
+    }
     setCubes((prevCubes) => [...prevCubes, newCube]);
   };
 
@@ -47,6 +143,7 @@ export function PlaceableBlock() {
     // loading: false
     // longitude: -76.010453
   } = useGeolocation();
+  console.log("🌟🚨 ~ PlaceableBlock ~ loading", loading);
   // useXRFrame((time, xrFrame) => {
   //   if (!ref.current) {
   //     return;
@@ -60,16 +157,17 @@ export function PlaceableBlock() {
   //   ref.current.rotation = heading;
   // });
 
-  //! ? reposition the block on interval according to camera position?
+  useInteraction(ref, "onSelect", () => console.log("selected!"));
 
-  return loading ? (
-    <Html ref={ref}>
-      <Icon>
-        <LocationOff />
-      </Icon>
-    </Html>
-  ) : (
+  return (
     <>
+      {loading ? (
+        <Html>
+          <Icon>
+            <LocationOff />
+          </Icon>
+        </Html>
+      ) : null}
       <Interactive
         onHover={() => {
           setIsHovered(true);
@@ -102,9 +200,9 @@ export function PlaceableBlock() {
   );
 }
 
-function getNearestGridPosition(position) {
-  const x = Math.round(position[0] / BOX_WIDTH) * BOX_WIDTH;
-  const y = Math.round(position[1] / BOX_WIDTH) * BOX_WIDTH;
-  const z = Math.round(position[2] / BOX_WIDTH) * BOX_WIDTH;
+function getNearestGridPosition([a, b, c]) {
+  const x = Math.round(a / BOX_WIDTH) * BOX_WIDTH;
+  const y = Math.round(b / BOX_WIDTH) * BOX_WIDTH;
+  const z = Math.round(c / BOX_WIDTH) * BOX_WIDTH;
   return [x, y, z];
 }
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,1 +1,1 @@
-export const BOX_WIDTH = 0.1;
+export const BOX_WIDTH = 1;
